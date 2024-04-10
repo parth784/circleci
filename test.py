6c42ec5c9cb33d4c66e76faa97fc708437c576e7
Parth Tammiwar <ptammiwar94@gmail.com>
2024-04-10 15:16:23 +0530

    succesful

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from main import to_upper
 
 class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
-        name = "parthfh"
+        name = "parth"
         upper_name = to_upper(name)
         self.assertEqual(upper_name, "PARTH")
 
