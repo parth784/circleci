f54ce5b90a6ada479ac78cc0c09ce279b53cadef
Parth Tammiwar <ptammiwar94@gmail.com>
2024-04-10 14:12:39 +0530

    built_test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name = "parth"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "PARTH")
+        self.assertEqual(upper_name, "PARTHTAMMIWAR")
 
 
 if __name__ == '__main__':
