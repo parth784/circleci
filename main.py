c3fe19873f3d4f21ebca36d30694a458aa0ea644
Parth Tammiwar <ptammiwar94@gmail.com>
2024-04-10 13:59:27 +0530

    testing_3

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# main.py
+def to_upper(name):
+    return name.upper()
 
-def add_numbers(a, b):
-    """Add two numbers together."""
-    return a + b
 
-def main():
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    result = add_numbers(num1, num2)
-    print("The result is:", result)
+def say_hello(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-if __name__ == "__main__":
-    main()
-print("hii")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    name = 'TrainWithShubham'
+    say_hello(name)
+    up = to_upper(name)
+    print(up)
--- test.py
+++ test.py
@@ -1,22 +1,13 @@
-# test_main.py
-
 import unittest
-from circleci.main import add_numbers
+from main import to_upper
 
-class TestAddNumbers(unittest.TestCase):
 
-    def test_add_numbers(self):
-        # Test case 1: Positive numbers
-        self.assertEqual(add_numbers(5, 3), 8)
+class MyTestCase(unittest.TestCase):
+    def test_to_upper(self):
+        name = "Shubham"
+        upper_name = to_upper(name)
+        self.assertEqual(upper_name, "SHUBHAM")
 
-        # Test case 2: Negative numbers
-        self.assertEqual(add_numbers(-5, -3), -8)
-
-        # Test case 3: Positive and negative numbers
-        self.assertEqual(add_numbers(5, -3), 2)
-
-        # Test case 4: Zero
-        self.assertEqual(add_numbers(0, 0), 0)
 
 if __name__ == '__main__':
     unittest.main()
